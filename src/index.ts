84a787f056d2a462a762f9e4f85bbeecb600f5af
Kryštof Woldřich <krystof.woldrich@sentry.io>
2022-10-04 20:36:30 +0200

    WORKING NATIVE MODULE (OLD_ARCH)

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@
 import { NativeModules } from 'react-native'
 
 export default NativeModules.Calculator;
+
+//
+// At this point the library is ready to be used.
+// Just create a new project and import it.
+// (This probably will stop working down the line
+// when the default modules will be Turbo Module):
+//
