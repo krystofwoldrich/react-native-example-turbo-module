1fd942e84bc9dec4ea7fca0bd42312e342f1a08c
Kryštof Woldřich <krystof.woldrich@sentry.io>
2022-10-04 19:58:26 +0200

    Add xcode project - static library

---
+++ ios/RNCalculator/RNCalculator/RNCalculator.h
@@ -0,0 +1,12 @@
+//
+//  RNCalculator.h
+//  RNCalculator
+//
+//  Created by Krystof Woldrich on 04.10.22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCalculator : NSObject
+
+@end
---
+++ ios/RNCalculator/RNCalculator/RNCalculator.m
@@ -0,0 +1,12 @@
+//
+//  RNCalculator.m
+//  RNCalculator
+//
+//  Created by Krystof Woldrich on 04.10.22.
+//
+
+#import "RNCalculator.h"
+
+@implementation RNCalculator
+
+@end
